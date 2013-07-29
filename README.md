0469ac8b3e7cd7780c900348d8c3b7d68a782ad0
Ric Allinson <capecodehq@yahoo.co.uk>
2013-07-29 15:56:49 -0700

    First commit

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
-php-render-mu
-=============
+# Php-render-mu
+
+Buffered renderer for passing Arrays into [Mustache](http://mustache.github.io/) templates.
+
+* Built with [mustache.php](https://github.com/bobthecow/mustache.php)
---
+++ index.php
@@ -0,0 +1,21 @@
+<?php
+namespace php_require\php_render_mu;
+
+require(__DIR__. "/mu/src/mustache/Autoloader.php");
+Mustache_Autoloader::register();
+
+$m = new Mustache_Engine();
+
+$module->exports = function ($filename, $data, $callback) use ($m) {
+
+    ob_start();
+    // extract($data);
+
+    if (isset($data["start"])) {
+        $data["end"] = (microtime(true) - $data["start"]) / 1000;
+    }
+
+    $buffer = $m->render(file_get_contents($filename), $data);
+    ob_end_clean();
+    $callback(null, $buffer);
+};
